66ace754fef6db5b1481ce763558049536e6637c
sevvian <59226487+sevvian@users.noreply.github.com>
2020-10-23 00:28:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+sed -i "s/PORT/$PORT/g" /etc/v2ray/config.json
 #sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 
 mkdir -p /var/tmp/nginx/client_body
